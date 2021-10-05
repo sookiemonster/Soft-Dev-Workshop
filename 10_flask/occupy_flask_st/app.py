53fb1a156aae4c6e045dfe9cdb016abd974307fe
sookiemonster <sookiemonster216@gmail.com>
2021-10-04 21:32:11 -0400

    Update 10_flask app.py

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -8,34 +8,55 @@ from random import choices
 
 app = Flask(__name__)
 
-def getRandomOccupation():
+def makeDict(filename):
 
-    occ_dict = {} # Create a dictionary to be filled in
-    filename = 'occupations.csv' # Specifies the target .csv file
+    dict = {}
 
     try:
         with open(filename) as csvfile:
             reader = DictReader(csvfile)
             for row in reader: 
-                # Fill in the dictionary with Job Classes as keys & Percentages as values
+                dict[row['Job Class']] = float(row['Percentage']) 
+
+        if 'Total' in dict.keys(): 
+            dict.pop('Total')
+
+    except FileNotFoundError: 
+        pass
+
+    return dict
+
+def getRandomKey(dictionary):
+
+    occ_dict = {} # Create a dictionary to be filled in
+    filename = 'occupations.csv'
+
+    try:
+        with open(filename) as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader: 
                 occ_dict[row['Job Class']] = float(row['Percentage']) 
 
         if 'Total' in occ_dict.keys(): 
-            occ_dict.pop('Total') # Remove the Total at the end of the .csv file if it exists
+            occ_dict.pop('Total')
 
-        # Get the first item in a list that is length k (in this case 1)
-        # The randomness of a key appearing is weighted based on its respective value
         result = choices(list(occ_dict.keys()), weights=occ_dict.values(), k=1)[0]
-        return(result)
+        return "<span style='display: block; margin-bottom: 10px; font-weight: bold'>Selected: %s</span>" % result
 
     except FileNotFoundError: 
-        print('File "%s" does not exist' % (filename)) 
         return 'File "%s" does not exist' % (filename)
 
+def listKeys(dictionary):
+    key_list = "<div>Occupations: "
+    for key in dictionary.keys():
+        key_list += "<p style='margin: 7px 20px'>%s</p>" % (key)
+    return key_list + "</div>"
 
 @app.route("/") 
 def main():
-    return str(getRandomOccupation())
+    occ_dict = makeDict('occupations.csv')
+    header = "<h3>The Red Imposter | Daniel Sooknanan, Roshani Shrestha, Shadman Rakib</h3>"
+    return header + getRandomKey(occ_dict) + listKeys(occ_dict)
 
 if __name__ == "__main__":
     app.run()
