d70ada7fe0724c4978d2b2e2af7ae3df1ae6646e
sookiemonster <sookiemonster216@gmail.com>
2021-10-18 19:48:16 -0400

    Add logout to 15_session

--- 15_session/app.py
+++ 15_session/app.py
@@ -3,12 +3,14 @@
 # K15 Flask Session Shenaniganza
 # 2021-10-18
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)    #create Flask object
 
 real_username = "username"
 real_password = "password"
+
+app.secret_key = 'BAD_SECRET_KEY' #
 
 @app.route("/", methods=['GET', 'POST'])
 def welcome():
@@ -17,13 +19,19 @@ def welcome():
         password = request.form.get('password')
         if real_username == username:
             if real_password == password:
-                return render_template( 'response.html', username = username)
+                session['username'] = request.form.get('username')
+                return render_template( 'response.html')
             else: 
                 return render_template( 'login.html', error = "Password is incorrect")
         else: 
             return render_template( 'login.html', error = "User does not exist")
     else:
         return render_template('login.html') # Redirect users to login if there's no post request to get info from
+
+@app.route("/logout")
+def logout():
+    session.pop('username', default=None)
+    return redirect("/")
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
